08f80bf87d2cfb657e6192d7e25509b7697902ba
DuParkLinux <dupark3@gmail.com>
2018-06-26 14:36:32 -0400

    Dynamic programming solution, storing the number of possibilities from beginning to end

--- 091DecodeWays/main.cpp
+++ 091DecodeWays/main.cpp
@@ -1,44 +1,46 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     int numDecodings(const string& s) {
-        if (s.empty()){
+        int size = s.length() + 1;
+        if (size == 1 || s[0] == '0'){
             return 0;
+        } else if (size == 2){
+            return 1;
         }
-        
-        int total = 0;
-        numDecodings(s, total);
+                
+        vector<int> possibilities(size, 0);
+        possibilities[0] = 1;
+        possibilities[1] = 1;
 
-        return total;
-    }
-
-    void numDecodings(const string& s, int& total){
-        // if empty, add one (at the end of recursive calls)
-        if (s.empty()){
-            ++total;
-            return ;
-        }
-
-        // if one char possible, call recursion without that letter
-        if (s.size() >= 1 && s[0] != '0'){
-            numDecodings(s.substr(1), total);
-            
-            // if two char possible (26 or under), call recursion without those two
-            if (s.size() >= 2 && stoi(s.substr(0,2)) <= 26){
-                numDecodings(s.substr(2), total);
+        for (int i = 2; i < size; ++i){
+            if (s[i-1] >= '1' && s[i-1] <= '9'){
+                possibilities[i] += possibilities[i-1];
+            }
+            if (s[i-2] == '1' || (s[i-2] == '2' && s[i-1] >= '0' && s[i-1] <= '6')){
+                possibilities[i] += possibilities[i-2];
             }
         }
+        
+
+        return possibilities[size - 1];
     }
+
+    
 };
 
 int main(){
     Solution s;
-    // 1 7 8 7 8 9 7 7 5 9 9 6 6 2 6
-    // 17 8 7 8 9 7 7 5 9 9 6 6 26
+    // # of digits = 4 
+    // # of tens = 2
+    // # of overlapping tens = 2
+    // 
+    // answer : 3          0123
+    cout << s.numDecodings("22619920") << endl;
     cout << s.numDecodings("1787897759966261825913315262377298132516969578441236833255596967132573482281598412163216914566534565") << endl;
     cout << s.numDecodings("1726") << endl;
     cout << s.numDecodings("002") << endl;
