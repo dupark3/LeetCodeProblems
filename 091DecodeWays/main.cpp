e50d9af58812d5cd28675155b20d714a15ab796f
DuParkLinux <dupark3@gmail.com>
2018-06-26 07:12:01 -0400

    Recursive calls by taking one or two chars off from the front. Add one to total number of possibilities if we have no chars left in the string when recursive call made.

--- 091DecodeWays/main.cpp
+++ 091DecodeWays/main.cpp
@@ -5,14 +5,43 @@ using namespace std;
 
 class Solution {
 public:
-    int numDecodings(string s) {
-        int answer;
+    int numDecodings(const string& s) {
+        if (s.empty()){
+            return 0;
+        }
+        
+        int total = 0;
+        numDecodings(s, total);
+
+        return total;
+    }
+
+    void numDecodings(const string& s, int& total){
+        // if empty, add one (at the end of recursive calls)
+        if (s.empty()){
+            ++total;
+            return ;
+        }
+
+        // if one char possible, call recursion without that letter
+        if (s.size() >= 1){
+            numDecodings(s.substr(1), total);
+        }
+
+        // if two char possible (26 or under), call recursion without those two
+        if (s.size() >= 2 && stoi(s.substr(0,2)) <= 26){
+            numDecodings(s.substr(2), total);
+        }
         
     }
 };
 
 int main(){
     Solution s;
-    s.numDecodings("226");
+    
+    cout << s.numDecodings("52") << endl;
+    cout << s.numDecodings("25") << endl;
+    cout << s.numDecodings("226") << endl;
+    cout << s.numDecodings("22652") << endl;
     return 0;
 }
