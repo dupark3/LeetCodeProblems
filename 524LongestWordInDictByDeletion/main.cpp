9400d9e82e71b7ec0d8de3195586c56d56c92eec
DuParkLinux <dupark3@gmail.com>
2018-07-02 15:21:03 -0400

    First commit

---
+++ 524LongestWordInDictByDeletion/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    string findLongestWord(const string& s, const vector<string>& d) {
+        
+    }
+};
+
+int main(){
+    Solution s;
+    s.findLongestWord("abpcplea", {"ale","apple","monkey","plea"});
+    return 0;
+}
