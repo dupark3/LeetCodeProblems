0010f3d2775a9633dafaa6df913af47d10739cdb
DuParkLinux <dupark3@gmail.com>
2018-07-04 14:31:02 -0400

    First working solution but time limit exceeded.

--- 084LargestRectangle/main.cpp
+++ 084LargestRectangle/main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <climits>
 #include <iostream>
 #include <vector>
 
@@ -6,16 +8,24 @@ using namespace std;
 class Solution {
 public:
     template <class T>
-    int localMax(T begin, T end){
-        
+    int localMin(T begin, T end){
+        int minimum = INT_MAX;
+        while (begin != end){
+            minimum = min(minimum, *begin);
+            ++begin;
+        }
+        // cout << "   local min : " << minimum << endl;
+        return minimum;
     }
 
     int largestRectangleArea(vector<int>& heights) {
         int max_width = heights.size();
         int max_area = 0;
         for (int i = max_width; i != 0; --i){
+            // cout << "i : " << i << endl;
             for (int j = 0; j != max_width - i + 1; ++j){
-                max_area = max(max_area, i * localMax(heights.begin() + j, heights.begin() + i));
+                max_area = max(max_area, i * localMin(heights.begin() + j, heights.begin() + j + i));
+                // cout << "   max_area = " << max_area << "   j = " << j << endl;
             }
         }
         return max_area;
@@ -25,6 +35,6 @@ public:
 int main(){
     Solution solution;
     vector<int> input = {2,1,5,6,2,3};
-    solution.largestRectangleArea(input);
+    cout << solution.largestRectangleArea(input) << endl;
     return 0;
 }
