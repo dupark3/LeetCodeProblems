360651deffeab1023a4e6f3d7a6f3146a169fc91
DuParkLinux <dupark3@gmail.com>
2018-07-06 06:52:47 -0400

    Accepted solution, beats 100% of submissions.

--- 695MaxAreaOfIsland/main.cpp
+++ 695MaxAreaOfIsland/main.cpp
@@ -7,8 +7,39 @@ using namespace std;
 
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        
+    int maxAreaOfIsland(vector< vector<int> >& grid) {
+        int width = grid[0].size();
+        int height = grid.size();
+        int max_area = 0;
+
+        for (int i = 0; i != height; ++i){
+            for (int j = 0; j != width; ++j){
+                if (grid[i][j] == 1){
+                    int new_area = 0;
+                    findLandArea(grid, new_area, i, j);
+                    max_area = max(max_area, new_area);
+                }
+            }
+        }
+        return max_area;
+    }
+
+    void findLandArea(vector< vector<int> >& grid, int& new_area, int i, int j){
+        grid[i][j] = 0;
+        ++new_area;
+
+        if (i > 0 && grid[i-1][j] == 1){
+            findLandArea(grid, new_area, i-1, j);
+        } 
+        if (i < grid.size() - 1 && grid[i+1][j] == 1){
+            findLandArea(grid, new_area, i+1, j);
+        }
+        if (j > 0 && grid[i][j-1] == 1){
+            findLandArea(grid, new_area, i, j-1);
+        }
+        if (j < grid[i].size() - 1 && grid[i][j+1]){
+            findLandArea(grid, new_area, i, j+1);
+        }
     }
 };
 
@@ -22,5 +53,7 @@ int main(){
                                    {0,0,0,0,0,0,0,0,0,0,1,0,0},
                                    {0,0,0,0,0,0,0,1,1,1,0,0,0},
                                    {0,0,0,0,0,0,0,1,1,0,0,0,0}};
+
+    cout << solution.maxAreaOfIsland(grid) << endl;
     return 0;
 }
