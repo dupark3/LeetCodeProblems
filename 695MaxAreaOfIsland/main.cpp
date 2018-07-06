7edd78f5be8abf9656ca93d64154f6db86da98b0
DuParkLinux <dupark3@gmail.com>
2018-07-05 20:12:44 -0400

    First commit, vector of vectors set to pass in grid.

---
+++ 695MaxAreaOfIsland/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        
+    }
+};
+
+int main(){
+    Solution solution;
+    vector< vector<int> > grid = { {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                                   {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                                   {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                                   {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                                   {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                                   {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                                   {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                                   {0,0,0,0,0,0,0,1,1,0,0,0,0}};
+    return 0;
+}
