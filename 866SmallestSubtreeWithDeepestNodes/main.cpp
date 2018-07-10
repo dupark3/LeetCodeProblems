8c37334f298b561e8f86f35d7228cb714adb104d
DuParkLinux <dupark3@gmail.com>
2018-07-10 10:31:48 -0400

    Recursively calculate left/right depth. if same depth on the left and right, current pointer contains all deepest nodes. if one side has greater depth, go to that node and do it again.

--- 866SmallestSubtreeWithDeepestNodes/main.cpp
+++ 866SmallestSubtreeWithDeepestNodes/main.cpp
@@ -1,5 +1,8 @@
+#include <algorithm>
+#include <climits>
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
 
@@ -12,7 +15,7 @@ struct TreeNode {
 };
  
 
-// BFS of printing
+// BFS of printing, nonmember just to support this problem
 void printTree(TreeNode* root){
     TreeNode* pointer = root;
     queue<TreeNode*> my_queue;
@@ -24,20 +27,56 @@ void printTree(TreeNode* root){
     while(!my_queue.empty()){
         if (my_queue.front()->left){
             my_queue.push(my_queue.front()->left);
-        }
+        } 
         if (my_queue.front()->right){
             my_queue.push(my_queue.front()->right);
-        }
+        } 
         
         cout << my_queue.front()->val << " ";
         my_queue.pop();
+    }
+
+    cout << endl;
+}
+
+int maxDepth(TreeNode* node){
+    if (!node){
+        return 0;
+    } else {
+        int left_depth = maxDepth(node->left);
+        int right_depth = maxDepth(node->right);
+        return max(left_depth, right_depth) + 1;
     }
 }
 
 class Solution {
 public:
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        // check max depth to the left and to the right
+        // if equal, return that node
+        // if left is greater, pointer = root->left, similar for right side
+        if (!root){
+            return 0;
+        }
+
+        TreeNode* pointer = root;
+        while (pointer){
+            // get left max depth
+            int left_depth = maxDepth(pointer->left);
+
+            // get right max depth
+            int right_depth = maxDepth(pointer->right);
+
+            // compare and reset pointer or return current node
+            if (left_depth == right_depth){
+                return pointer;
+            } else {
+                pointer = left_depth > right_depth ? pointer->left : pointer->right;
+            }
+        }
         
+
+        return pointer;
     }
 };
 
@@ -51,6 +90,8 @@ int main(){
     TreeNode eight(8);
     TreeNode seven(7);
     TreeNode four(4);
+    TreeNode ten(10);
+
 
     root.left = &five;
     root.right = &one;
@@ -60,13 +101,14 @@ int main(){
     one.right = &eight;
     two.left = &seven;
     two.right = &four;
+    seven.left = &ten;
 
     printTree(&root);
 
     Solution solution;
     TreeNode* subtree = solution.subtreeWithAllDeepest(&root);
 
-    TreeNode* pointer = subtree;
+    printTree(subtree);
     
 
 }
