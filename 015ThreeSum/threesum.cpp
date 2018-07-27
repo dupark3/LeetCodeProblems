487dc33466937c6e8f0321d8b600d1a6796ab1c0
DuParkLinux <dupark3@gmail.com>
2018-07-27 15:30:37 -0400

    Accepted solution, beats 15%. Improved from 13%... Sort vector first then iterate through pairs, looking for its complement. Do some skipping based on repeat values on the input vector.

--- 015ThreeSum/threesum.cpp
+++ 015ThreeSum/threesum.cpp
@@ -1,44 +1,52 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int numsSize = nums.size();
         vector<vector<int>> result;
         
-        if (numsSize < 3){
+        if (nums.size() < 3){
             return result;
         }
         
         sort(nums.begin(), nums.end());
         
-        for (int i = 0; i != numsSize; ++i){
-            for (int j = i + 1; j != numsSize; ++j){
-                for (int k = j + 1; k != numsSize; ++k){
-                    if (nums[i] + nums[j] + nums[k] == 0){
-                        vector<int> temp = {nums[i], nums[j], nums[k]};
-                        if (find(result.begin(), result.end(), temp) == result.end()){
-                            result.push_back(temp);  
-                        }
-                    } 
-                        
+        for (auto i = nums.begin(); i + 2 != nums.end(); ++i){
+            //cout << "for loop" << endl;
+            if (i != nums.begin() && *i == *(i - 1)){
+                // if first number is the same starting point as before, ignore this starting point
+                continue;
+            }
+            for (auto j = i + 1; j + 1 != nums.end(); ++j){
+                while (j != nums.end() && *j == *(j - 1) && (j - 1) != i){
+                    // if second number is the same starting point as before,
+                    ++j;
                 }
+                if (j == nums.end()){
+                    break;
+                }
+                //auto k = my_binary_search(j + 1, nums.end(), (*i + *j) * -1, nums.end());
+                if (binary_search(j+1, nums.end(), (*i + *j) * -1)){
+                    result.push_back({*i, *j, (*i + *j) * -1});
+                }
+                
             }
         }
+
         return result;
     }
 };
 
 int main() {
-    vector<int> input = {-1, 0, 1, 2, -1, -4};
-    // -4 -1 -1 0 1 2
+    vector<int> input = {0, 0, 0, 0};
+    vector<int> input2 = {1, 1, -2, -3, 3, 0, 2, 2, -5, 10, -10, 100, -50, -50};
     Solution solution;
-    vector<vector<int>> answer = solution.threeSum(input);
+    vector<vector<int>> answer = solution.threeSum(input2);
 
     for (int i = 0; i != answer.size(); ++i){
         for (int j = 0; j != answer[i].size(); ++j){
