00ddc2ce1439c704b31e272313c2de6bd08d701a
DuParkLinux <dupark3@gmail.com>
2018-08-11 00:18:52 -0400

    Accepted solution using O(1) space by reversing two halves then reversing the whole thing

--- 189RotateArray/rotate.cpp
+++ 189RotateArray/rotate.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -7,33 +8,32 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int size = nums.size();
-        vector<int> result;
-        if (k > size){
-            k = k % size;
-        }
-        for (int i = size - k; i != size; ++i){
-            result.push_back(nums[i]);
-        }
-        for (int i = 0; i != size - k; ++i){
-            result.push_back(nums[i]);
-        }
-        nums = result;
+        k = k % size;
+        
+        // reverse last k elements
+        reverse(nums.begin() + (size - k), nums.end());
+
+        // reverse first size - k elements
+        reverse(nums.begin(), nums.begin() + (size - k));
+
+        // reverse whole thing
+        reverse(nums.begin(), nums.end());
     }
 };
 
 int main() {
     Solution solution;
-    vector<int> nums = {1,2,3,4,5,6,7};
+    vector<int> nums = {1,2,3,4,5,6}; // 6 2 3 4 5 1
     for (int i = 0; i != nums.size(); ++i){
         cout << nums[i] << " ";
     }
     cout << endl;
 
-    solution.rotate(nums, 9);
+    solution.rotate(nums, 4);
     for (int i = 0; i != nums.size(); ++i){
         cout << nums[i] << " ";
     }
     cout << endl;
-    
+
     return 0;
 }
