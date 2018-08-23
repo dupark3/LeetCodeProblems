7d77012a397084b96003efbfc9f6b310e5a664f2
DuParkLinux <dupark3@gmail.com>
2018-08-23 15:13:38 -0400

    optimized to beat 57.61%. Initialize visited vector flags using constructor.

--- 079WordSearch/search.cpp
+++ 079WordSearch/search.cpp
@@ -6,67 +6,67 @@ using namespace std;
 
 class Solution {
 public:
-    bool DFS(vector<vector<char>>& board, 
-             string word, 
+    bool DFS(const vector<vector<char>>& board, 
+             const string& word, 
              int i, int j, 
              int word_index, 
              vector<vector<bool>>& visited)
     {
-        cout << "checking " << board[i][j] << " with " << word[word_index] << endl;
         visited[i][j] = true;
         if (word_index + 1 == word.size()) return true;
 
+        // if horizontal/vertical neighbor exists,
+        // if that neighbor matches the next letter,
+        // and if that neighbor has not been visited:
+        // DFS to that neighbor
         if (i + 1 < board.size() && 
-            visited[i+1][j] == false && 
             board[i+1][j] == word[word_index + 1] &&
+            visited[i+1][j] == false && 
             DFS(board, word, i+1, j, word_index+1, visited)){
             return true;
         }
         if (i - 1 >= 0 && 
-            visited[i-1][j] == false &&
             board[i-1][j] == word[word_index + 1] &&
+            visited[i-1][j] == false &&
             DFS(board, word, i-1, j, word_index+1, visited)){
             return true;
         }
         if (j + 1 < board[0].size() && 
-            visited[i][j+1] == false &&
             board[i][j+1] == word[word_index + 1] &&
+            visited[i][j+1] == false &&
             DFS(board, word, i, j+1, word_index+1, visited)){
             return true;
         }
         if (j - 1 >= 0 && 
-            visited[i][j-1] == false &&
             board[i][j-1] == word[word_index + 1] &&
+            visited[i][j-1] == false &&
             DFS(board, word, i, j-1, word_index+1, visited)){
             return true;
         }
 
+        // if DFS fails, mark it as un-visited for other DFS traversals
         visited[i][j] = false;
         return false;
     }
 
-    bool exist(vector<vector<char>>& board, string word) {
+    bool exist(const vector<vector<char>>& board, const string& word) {
         int rows = board.size();
         int columns = 0;
-        if (rows > 0) columns = board[0].size();
-        if (rows == 0 || columns == 0) return false;
-        if (word.size() == 0) return true;
+        // set column size
+        if (rows > 0)                  columns = board[0].size();
 
-        vector<vector<bool>> visited;
-        visited.resize(rows);
-        for (int i = 0; i != rows; ++i){
-            visited[i].resize(columns);
-        }
+        // exit if size 0 for any inputs
+        if (rows == 0 || columns == 0) return false;
+        if (word.size() == 0)          return true;
+
+        // flags to indicate visited
+        vector<vector<bool>> visited(rows, vector<bool>(columns, 0));
 
         // DFS 
-        for (int i = 0; i != rows; ++i){
-            for (int j = 0; j != columns; ++j){
-                cout << "i : " << i << " j: " << j << endl;
-                if (board[i][j] == word[0] && DFS(board, word, i, j, 0, visited)){
+        for (int i = 0; i != rows; ++i)
+            for (int j = 0; j != columns; ++j)
+                if (board[i][j] == word[0] && DFS(board, word, i, j, 0, visited))
                     return true;
-                } 
-            }
-        }
 
         return false;
     }
