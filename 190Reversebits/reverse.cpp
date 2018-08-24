bb76e882b9ab665e6612e03b27bb0487f2d11a64
DuParkLinux <dupark3@gmail.com>
2018-08-24 07:22:05 -0400

    Clearing bits by shifting to the left, right, then left...

---
+++ 190Reversebits/reverse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int result = 0; 
+        for (int i = 0; i != 32; ++i){
+            result += (n << 31 - i) >> 31 << (31 - i);
+            // result += ((n >> i) << 31 >> 31 << (31 - i));
+        }
+        return result;
+    }
+};
+
+int main(){
+    Solution solution;
+    cout << solution.reverseBits(43261596) << endl;
+    return 0;
+}
