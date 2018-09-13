6a007279edd32480e79a2126fdbb434896f12873
Du Park <dupark3@gmail.com>
2018-09-13 15:30:44 -0400

    Accepted solution, added another exit condition. Find the left most target index using firstBinarySearch, then find the right most target index using lastBinarySearch. Two binary searches will make it O(logn) at worst, instead of finding one index and looking for left and right bound from that index (if the entire array is the target number, it would require O(n) time to find the left and right bounds).

--- 034FindTargetRange/find.cpp
+++ 034FindTargetRange/find.cpp
@@ -8,22 +8,26 @@ using namespace std;
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        // binary search fails = return {-1, -1}
+        if (nums.empty()){
+            return {-1, -1};
+        }
+
         int left = 0;
         int right = nums.size();
 
-        // binary search for first occurance of target
+        // find the left most target and right most target, if any
         int first = firstBinarySearch(left, right, nums, target);
-
-        // binary search for last occurance of target
-        // int last = lastBinarySearch(left, right, nums, target);
-        // return {first, last};
-        return {first, first};
+        int last = lastBinarySearch(left, right, nums, target);
+        
+        return {first, last};
     }
 
     int firstBinarySearch(int left, int right, const vector<int>& nums, int target){
         while(true){
             int mid = left + (right - left) / 2;
+            if (right - left <= 1 && nums[mid] != target){
+                return -1;
+            }
             if (nums[mid] < target){
                 // going right
                 if (mid == nums.size() -1){
@@ -44,13 +48,35 @@ public:
     }
 
     int lastBinarySearch(int left, int right, const vector<int>& nums, int target){
-        
+        while(true){
+
+            int mid = left + (right - left) / 2;
+            if (right - left <= 1 && nums[mid] != target){
+                return -1;
+            }
+            if (nums[mid] > target){
+                // going left
+                if (mid == 0){
+                    return -1;
+                }
+                right = mid;
+            } else {
+                // going right or target found
+                if (mid == nums.size() - 1 && nums[mid] != target){
+                    return -1;
+                } else if (mid == nums.size() - 1 || (nums[mid] == target && nums[mid + 1] != target)){
+                    return mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
     }
 };
 
 int main(){
     Solution solution;
-    vector<int> nums = {5, 6, 8, 8, 8, 9, 9, 10};
-    vector<int> range = solution.searchRange(nums, 8);
+    vector<int> nums = {5, 6, 8, 8, 8, 9, 9, 11};
+    vector<int> range = solution.searchRange(nums, 10);
     cout << range[0] << ", " << range[1] << endl; // 2, 4
 }
