817482324d898fd20d3a2f3b6fd2f6d72bb8f957
Du Park <dupark3@gmail.com>
2018-09-13 14:08:01 -0400

    Cleaner solution, starting with right = x/2

--- 069Sqrt/sqrt.cpp
+++ 069Sqrt/sqrt.cpp
@@ -5,39 +5,22 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-        /*2147483648
-        46340 * 46340 = 2147395600
-        46340 / 2 = 23170*/
-        if (x == 0){
-            return 0;
-        }
-        int n = 23170;
-        int right = 46340;
-        int left = 0;
+        if (x <= 1)
+            return x;
+
+        int left = 1;
+        int right = x/2;
 
         while (true){
-/*            cout << "left  : " << left << endl;
-            cout << "right : " << right << endl;
-            cout << "n     : " << n << endl;*/
-            if (n * n == x){
-                return n;
-            } else if (n * n < x){
-                /*cout <<"    going right" << endl;*/
-                left = n;
-                int increment = (right - n) / 2;
-                if (increment == 0) {
-                    if ((n + 1) * (n + 1) > x)
-                        return n;
-                    else
-                        return n + 1;
-                }
-                n += increment;
-            } else if (n * n > x){
-                /*cout <<"    going left" << endl;*/
-                right = n;
-                int increment = (n - left) / 2;
-                if (increment == 0) return n - 1;
-                n -= increment;
+            int mid = left + ((right - left) / 2);
+            if (mid > x / mid){  
+                // going left
+                right = mid;
+            } else {
+                // going right
+                if ((mid + 1) > x / (mid + 1))
+                    return mid;
+                left = mid + 1;
             }
         }
     }
